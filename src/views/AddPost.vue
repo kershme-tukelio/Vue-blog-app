e9ba6c6c53d0d28a501a0b28c9887942b0853f6f
Jovan Zoric <jzjovanzoric@gmail.com>
2021-01-29 20:38:34 +0100

    Objective 6 complete

--- src/views/AddPost.vue
+++ src/views/AddPost.vue
@@ -2,9 +2,9 @@
     <div>
         <form @submit.prevent="onSubmit" >
             <label for="title">Title: </label> <br/>
-            <input v-model="formData.title" name="title" type="text" id="title" > <br/><br/>
+            <input required :minlength="2" v-model="formData.title" name="title" type="text" id="title" > <br/><br/>
             <label for="text">Text: </label> <br/>
-            <textarea name="text" v-model="formData.text" id="text" cols="30" rows="10"></textarea> <br/><br/>
+            <textarea required :maxlength="300" name="text" v-model="formData.text" id="text" cols="30" rows="10"></textarea> <br/><br/>
             <button type="reset">Reset form</button>
             <button type="submit"> Submit </button>
         </form>
